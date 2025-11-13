7083a74385e1c9088b7fa4f1a7baf866d6b0c62c
Wizaaaa <jakubsalaky@gmail.com>
2025-11-13 17:10:19 +0100

    State hoist isClicked in Card.kt to CreateScreen.kt and some base of ClickCard.kt and AlertDialog

--- app/src/main/java/com/example/rocnikova_prace/ui/CreateScreen.kt
+++ app/src/main/java/com/example/rocnikova_prace/ui/CreateScreen.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.rocnikova_prace.R
 import com.example.rocnikova_prace.ui.components.Cards
+import com.example.rocnikova_prace.ui.components.ClickCard
 import com.woowla.compose.icon.collections.heroicons.Heroicons
 import com.woowla.compose.icon.collections.heroicons.heroicons.Outline
 import com.woowla.compose.icon.collections.heroicons.heroicons.outline.AcademicCap
@@ -20,22 +25,47 @@ fun CreateScreen() {
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ) {
+        var showDialog by remember { mutableStateOf(false) }
+        var isClicked by remember { mutableStateOf(false) }
+
         Cards(
             text = R.string.new_questions,
-            icon = Heroicons.Outline.Plus
+            icon = Heroicons.Outline.Plus,
+            isClicked = isClicked,
+            onClick = {
+                isClicked = !isClicked
+                showDialog = !showDialog
+            }
         )
 
         Cards(
             text = R.string.schools_questions,
-            icon = Heroicons.Outline.AcademicCap
+            icon = Heroicons.Outline.AcademicCap,
+            isClicked = isClicked,
+            onClick = {
+                isClicked = !isClicked
+                showDialog = !showDialog
+            }
         )
 
         Cards(
             text = R.string.import_questions,
-            icon = Heroicons.Outline.ArrowDownTray
+            icon = Heroicons.Outline.ArrowDownTray,
+            isClicked = isClicked,
+            onClick = {
+                isClicked = !isClicked
+                showDialog = !showDialog
+            }
         )
 
+        if (showDialog) {
+            ClickCard(onDismissRequest = { onDismissRequest() })
+        }
+
     }
+}
+
+private fun onDismissRequest() {
 
 }
 
--- app/src/main/java/com/example/rocnikova_prace/ui/components/Card.kt
+++ app/src/main/java/com/example/rocnikova_prace/ui/components/Card.kt
@@ -4,7 +4,6 @@ import androidx.annotation.StringRes
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,32 +17,23 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.rocnikova_prace.R
-import com.woowla.compose.icon.collections.heroicons.Heroicons
-import com.woowla.compose.icon.collections.heroicons.heroicons.Outline
-import com.woowla.compose.icon.collections.heroicons.heroicons.outline.Plus
 
 
 @Composable
 fun Cards(
     icon: ImageVector,
     @StringRes text: Int,
+    onClick: () -> Unit,
+    isClicked: Boolean,
     modifier: Modifier = Modifier
 ) {
-    var isClicked by remember { mutableStateOf(false) }
-
     val elevation by animateDpAsState(
         targetValue = if (isClicked) 2.dp else 8.dp,
         animationSpec = tween(durationMillis = 150)
@@ -55,7 +45,7 @@ fun Cards(
     )
 
     ElevatedCard(
-        onClick = { isClicked = !isClicked },
+        onClick = onClick,
         colors = CardDefaults.elevatedCardColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
@@ -63,15 +53,17 @@ fun Cards(
         modifier = modifier
             .fillMaxWidth()
             .padding(20.dp)
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onPress = {
-                        isClicked = true
-                        tryAwaitRelease()  // čeká, dokud uživatel nepustí prst
-                        isClicked = false
-                    }
-                )
-            }
+
+            // TODO: Get this out od card
+//            .pointerInput(Unit) {
+//                detectTapGestures(
+//                    onPress = {
+//                        isClicked = true
+//                        tryAwaitRelease()  // čeká, dokud uživatel nepustí prst
+//                        isClicked = false
+//                    }
+//                )
+//            }
             .graphicsLayer(
                 scaleX = scale,
                 scaleY = scale
@@ -100,14 +92,4 @@ fun Cards(
             Spacer(modifier = Modifier.weight(1f))
         }
     }
-}
-
-
-@Preview
-@Composable
-fun PreviewCard() {
-    Cards(
-        text = R.string.new_questions,
-        icon = Heroicons.Outline.Plus
-    )
 }
